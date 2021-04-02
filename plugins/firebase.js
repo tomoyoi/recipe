0177aec6be03d8bc60800cf50e8f0e44293566f3
Tomoyo Ito <tmyooo73@gmail.com>
2021-04-02 16:49:22 +0900

    add

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,13 +1,18 @@
 import firebase from 'firebase'
 
-//firebaseの初期化に必要なデータをconfigで用意する
-//必要に応じてパラメーターをここで定義
-const config = {
-  projectId: process.env.FIREBASE_PROJECT_ID
-}
-//二重に初期化が行われないために!firebase.app.lengthで確認をする
-if (firebase.apps.length === 0) {
-  firebase.initializeApp(config)
+if (!firebase.apps.length) {
+  firebase.initializeApp(
+    {
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      apiKey: "AIzaSyAATEW0bGxlCRWjMatijMpfE8Pbv42Fao0",
+      authDomain: "cloud-vision-24d73.firebaseapp.com",
+      projectId: "cloud-vision-24d73",
+      storageBucket: "cloud-vision-24d73.appspot.com",
+      messagingSenderId: "324343967564",
+      appId: "1:324343967564:web:c52b56c745642776cae9e9",
+      measurementId: "G-CCCCPFNV7V"
+    }
+  )
 }
 
 export default firebase
